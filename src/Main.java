a76e04bec3e623bb81e07746bbe2285c5a28f9fb
Pouya <pouyamohammadyirbu@gmail.com>
2024-02-09 11:52:05 +0330

    traversDF - BF - Graph

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,24 @@ import ds.non_linear.Graph;
 public class Main {
     public static void main(String[] args) {
         Graph graph = new Graph();
+
         graph.addNode("A");
         graph.addNode("B");
         graph.addNode("C");
         graph.addNode("D");
+        graph.addNode("E");
+
         graph.addEdge("A", "B");
-        graph.addEdge("A", "D");
+        graph.addEdge("A", "E");
         graph.addEdge("C", "A");
-        graph.addEdge("D", "B");
+        graph.addEdge("C", "B");
+        graph.addEdge("C", "D");
+        graph.addEdge("B", "E");
+        graph.addEdge("D", "E");
 
         graph.print();
+
+        graph.traversBreadthFirst("C");
+
     }
 }
--- src/ds/non_linear/Graph.java
+++ src/ds/non_linear/Graph.java
@@ -1,5 +1,6 @@
 package ds.non_linear;
 
+import javax.crypto.spec.DESedeKeySpec;
 import java.util.*;
 
 public class Graph {
@@ -67,6 +68,69 @@ public class Graph {
             return;
 
         edges.get(fromNode).remove(toNode);
+    }
+
+    private void traversDepthFirstRecursive(Node root, Set<Node> visited){
+        System.out.println(root);
+        visited.add(root);
+
+        for (var neighbor : edges.get(root))
+            if(!visited.contains(neighbor))
+                traversDepthFirstRecursive(neighbor, visited);
+    }
+
+    private void traversDepthFirstIterative(Node root){
+        Set<Node> visited = new HashSet<>();
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()){
+            var current = stack.pop();
+            System.out.println(current);
+            visited.add(current);
+            for (var neighbor : edges.get(current))
+                if (!visited.contains(neighbor))
+                    stack.push(neighbor);
+        }
+
+    }
+
+    public void traversDepthFirst(String start){
+        var node = vertices.getOrDefault(start, null);
+        if (node == null)
+            return;
+        System.out.println("Recursive:");
+        traversDepthFirstRecursive(node, new HashSet<>());
+        System.out.println("Iterative:");
+        traversDepthFirstIterative(node);
+    }
+
+
+    private void traversBreadthFirst(Node root){
+        Set<Node> visited = new HashSet<>();
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()){
+            var current = queue.remove();
+
+            if (visited.contains(current)) continue;
+
+            System.out.println(current);
+            visited.add(current);
+
+            for (var neighbor : edges.get(current))
+                if (!visited.contains(neighbor))
+                    queue.add(neighbor);
+
+        }
+    }
+
+    public void traversBreadthFirst(String start){
+        var node = vertices.getOrDefault(start, null);
+        if (node == null)
+            return;
+        traversBreadthFirst(node);
     }
 
     public void print(){
