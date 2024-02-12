2d275023894f043739853a520089aa0f93799069
Pouya <pouyamohammadyirbu@gmail.com>
2024-02-12 17:09:49 +0330

    Merge Sort

--- src/algorithm/Sort.java
+++ src/algorithm/Sort.java
@@ -36,6 +36,43 @@ public class Sort {
                     break;
     }
 
+    public static void mergeSort(int[] array){
+        if (array.length < 2)
+            return;
+
+        // divide the arrays
+        int middle = array.length / 2;
+        int[] left = new int[middle];
+        for (int i = 0; i < middle; i++)
+            left[i] = array[i];
+        int[] right = new int[array.length - middle];
+        for(int i = middle; i < array.length; i++)
+            right[i-middle] = array[i];
+
+        // sort each side
+        mergeSort(left);
+        mergeSort(right);
+
+        // merge them into one\
+        merge(left, right, array);
+    }
+
+    private static void merge(int[] left, int[] right, int[] res){
+        int i, j, k;
+        i=j=k=0;
+
+        while (i < left.length && j < right.length)
+            if (left[i] <= right[j])
+                res[k++] = left[i++];
+            else
+                res[k++] = right[j++];
+
+        while (i < left.length)
+            res[k++] = left[i++];
+        while (j < right.length)
+            res[k++] = right[j++];
+    }
+
     private static void swap(int first, int second, int[] array){
         if (first == second)
             return;
