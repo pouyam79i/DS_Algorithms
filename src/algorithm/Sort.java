b83bb8d6537ca67c6d09cdf43a7ee87b7ac0b94e
Pouya <pouyamohammadyirbu@gmail.com>
2024-02-12 11:36:35 +0330

    Selection Sort

--- src/algorithm/Sort.java
+++ src/algorithm/Sort.java
@@ -4,7 +4,7 @@ public class Sort {
 
     public static void bubbleSort(int[] array){
         boolean isSorted;
-        for(int i=0; i < array.length; i++){
+        for (int i=0; i < array.length; i++){
             isSorted = true;
             for (int j=1; j < array.length - i; j++)
                 if (array[j-1] > array[j]){
@@ -16,10 +16,23 @@ public class Sort {
         }
     }
 
+    public static void selectionSort(int[] array){
+        int minValueIndex;
+        for (int i = 0; i < array.length; i++){
+            minValueIndex = i;
+            for (int j = i + 1; j < array.length; j++)
+                if (array[j] < array[minValueIndex])
+                    minValueIndex = j;
+            swap(i, minValueIndex, array);
+        }
+    }
+
     private static void swap(int first, int second, int[] array){
-        array[first] += array[second];
+        if (first == second)
+            return;
+        array[first] = array[first] + array[second];
         array[second] = array[first] - array[second];
-        array[first] -= array[second];
+        array[first] = array[first] - array[second];
     }
 
 }
